23c942a99c1c56cb4bcad4cb184d464233979d0a
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-07-09 15:02:10 +0100

    Remove the comments from create_spec.js

--- spec/create_spec.js
+++ spec/create_spec.js
@@ -1,13 +1,3 @@
-//Test to create a note entry
-//Check that a title is created automatically
-//from a note entry
-//Check that a note has content
-
-//Create note is intialised with title
-//(first 20 chars of content)
-//and content
-//Potentially id created at later point
-
 (function(exports) {
 
   function testNoteHasContent() {
