d8200cda6bbc4482e0bf4b225d3a5d143fa45342
joseph townshend <joetownshend@mac.com>
2019-07-10 10:38:19 +0100

    remove id from create function and tests

--- spec/create_spec.js
+++ spec/create_spec.js
@@ -2,35 +2,26 @@
 
   function testNoteHasContent() {
     var note = new Note();
-    assert.isTrue(note.content === null)
+    assert.isTrue(note.content === null);
   };
 
   testNoteHasContent();
 
   function testNoteHasTitle() {
     var note = new Note();
-    assert.isTrue(note.title === null)
+    assert.isTrue(note.title === null);
   };
 
   testNoteHasTitle();
 
-  function testNoteHasId() {
-    var note = new Note();
-    assert.isTrue(note.id === 0)
-  };
-
-  testNoteHasId();
-  
   function testNoteCreation() {
     var note = new Note();
-    note.create("Lorem ipsum dolor sit amet.")
-    assert.isTrue(note.content === "Lorem ipsum dolor sit amet.")
-    assert.isTrue(note.title === "Lorem ipsum dolor si")
+    note.create("Lorem ipsum dolor sit amet.");
+    assert.isTrue(note.content === "Lorem ipsum dolor sit amet.");
+    assert.isTrue(note.title === "Lorem ipsum dolor si");
   };
 
   testNoteCreation();
-
-
 
 
 
--- src/create.js
+++ src/create.js
@@ -2,12 +2,11 @@
   function Note() {
     this.content = null;
     this.title = null;
-    this.id = 0;
   };
 
   Note.prototype.create = function(content) {
-    this.content = content
-    this.title = content.substring(0, 20)
+    this.content = content;
+    this.title = content.substring(0, 20);
   };
 
   exports.Note = Note;
