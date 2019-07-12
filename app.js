734c09ab366a1972a7b79d604e62d024d62687f2
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-07-12 13:48:18 +0100

    Updtes app file

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ window.addEventListener('load', function() {
   // this is where we listens to submit, creates note, adds note to list, updates DOM
   var controller = new Controller(noteView, noteModel);
 
+  // extracts the dependany on the 'app' id in index.html
+  var appIdElement = document.getElementById("app");
+
   // creates the app - updates DOM
-  var element = document.getElementById("app");
-  controller.updateDOM(element);
+  controller.updateDOM(appIdElement);
 })
